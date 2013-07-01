39cb3f41c7fa2f9b8df8514d519808fde7e7df5d
Cold Head <certainthings@gmail.com>
2013-07-01 15:28:15 +1000

    Routing

---
+++ routing/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Routing::Application.config.secret_token = '4dd9d819bc2e772f9179cb4a1f542c1db0f5275169fc69a92e558cf4e5eb81b8250d440a384a798d6cd713dfd22a34d2cee79d8663f5157b39f245cdbb18ce5f'
