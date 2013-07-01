c103c151550e2567cc52ecb7f0d29a3d48f4659b
Cold Head <certainthings@gmail.com>
2013-07-01 11:38:55 +1000

    Basic

---
+++ basic/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basic::Application.config.secret_token = '42bfc80f15dfcded7bc25a8b60e5ecae8f65c7fab4938428de9002a96eec594e635e9d1ab77ac4b3b92987119ec873bdb2bf98c49e3287864f5105f357de2101'
