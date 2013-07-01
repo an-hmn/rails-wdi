10e3c889447e3df7e11521832d96689cddef435e
Cold Head <certainthings@gmail.com>
2013-07-01 11:32:37 +1000

    Movies

---
+++ first-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4713626f0690d8ad8eaaa45ac8ce6f2ae2883c57d413ec7d3fc00e13dcaa7094274adcb000bb5c85340efc0d8804de55ea566aefa27df411bd5888620cf3eafd'
