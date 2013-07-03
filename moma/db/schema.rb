3db9807360d028eed38516091d038089c6e1cd23
Daniel T <tsui.daniel@gmail.com>
2013-07-03 18:15:57 +1000

    add artist_id to paintings table

--- moma/db/schema.rb
+++ moma/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130703013456) do
+ActiveRecord::Schema.define(:version => 20130703062300) do
 
   create_table "artists", :force => true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20130703013456) do
     t.text     "image"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "artist_id"
   end
 
 end
