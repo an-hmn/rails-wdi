e9ebcbcdcd19432e13757a994433fd482e7f7daa
Cold Head <certainthings@gmail.com>
2013-07-03 10:50:09 +1000

    Planets: Create,Update, Delete

--- planets/app/controllers/planets_controller.rb
+++ planets/app/controllers/planets_controller.rb
@@ -14,4 +14,20 @@ class PlanetsController < ApplicationController
   def show
     @planet = Planet.find(params[:id])
   end
+
+  def destroy
+    planet = Planet.find(params[:id])
+    planet.destroy
+    redirect_to(planets_path)
+  end
+
+  def edit
+    @planet = Planet.find(params[:id])
+  end
+
+  def update
+    planet = Planet.find(params[:id])
+    planet.update_attributes(params[:planet])
+    redirect_to(planets_path)
+  end
 end
