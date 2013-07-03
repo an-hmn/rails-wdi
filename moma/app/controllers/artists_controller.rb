d9866b6d6e4df3fc41d2fe26c3b7edf2ffe16bb8
Cold Head <certainthings@gmail.com>
2013-07-03 15:52:35 +1000

    Create Update, delete: ngs, artists

--- moma/app/controllers/artists_controller.rb
+++ moma/app/controllers/artists_controller.rb
@@ -17,11 +17,18 @@ class ArtistsController < ApplicationController
   end
 
   def edit
+    @artist = Artist.find(params[:id])
   end
 
   def update
+    artist = Artist.find(params[:id])
+    artist.update_attributes(params[:artist])
+    redirect_to(artist)
   end
 
   def destroy
+    artist = Artist.find(params[:id])
+    artist.destroy
+    redirect_to(artists_path)
   end
 end
--- moma/app/controllers/paintings_controller.rb
+++ moma/app/controllers/paintings_controller.rb
@@ -17,11 +17,18 @@ class PaintingsController < ApplicationController
   end
 
   def edit
+    @painting = Painting.find(params[:id])
   end
 
   def update
+    painting = Painting.find(params[:id])
+    painting.update_attributes(params[:painting])
+    redirect_to(painting)
   end
 
   def destroy
+    painting = Painting.find(params[:id])
+    painting.destroy
+    redirect_to(paintings_path)
   end
 end
