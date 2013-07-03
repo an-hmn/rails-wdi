bdd63beb8e1d176f2eceba4dfeaed80727f1cfeb
Cold Head <certainthings@gmail.com>
2013-07-03 12:03:02 +1000

    DB setup

---
+++ moma/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moma::Application.config.secret_token = '0077e5c40c403946ebb121f3a93e72c7753d274bec8b20c11877a11b969fc61421f0234ab2cc80be2220ee5fc92fca7ad76039342d06e575539b3439078e1b8f'
