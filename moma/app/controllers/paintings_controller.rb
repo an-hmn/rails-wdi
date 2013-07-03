43d6700416fc16f6a5cf808817e19d39583dd623
Cold Head <certainthings@gmail.com>
2013-07-03 12:58:25 +1000

    Controllers, first views

---
+++ moma/app/controllers/paintings_controller.rb
@@ -0,0 +1,23 @@
+class PaintingsController < ApplicationController
+  def index
+    @paintings = Painting.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
