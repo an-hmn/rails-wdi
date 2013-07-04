376ad6d8a6504e60509e44b680e608625e313bc3
Cold Head <certainthings@gmail.com>
2013-07-04 15:48:10 +1000

    Ingredients controller

--- cookbook/app/controllers/ingredients_controller.rb
+++ cookbook/app/controllers/ingredients_controller.rb
@@ -8,17 +8,27 @@ class IngredientsController < ApplicationController
   end
 
   def create
+    ingredient = Ingredient.create(params[:ingredient])
+    redirect_to(ingredient)
   end
 
   def new
+    @ingredient = Ingredient.new
   end
 
   def edit
+    @ingredient = Ingredient.find(params[:id])
   end
 
   def update
+    ingredient = Ingredient.find(params[:id])
+    ingredient.update_attributes(params[:ingredient])
+    redirect_to(ingredient)
   end
 
   def destroy
+    ingredient = Ingredient.find(params[:id])
+    ingredient.destroy
+    redirect_to(ingredients_path)
   end
 end
