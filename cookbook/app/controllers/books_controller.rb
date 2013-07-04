49399fa4d5c39d73ded3b2812b69ac40a93430a4
Cold Head <certainthings@gmail.com>
2013-07-04 16:28:55 +1000

    CRUD for Books

--- cookbook/app/controllers/books_controller.rb
+++ cookbook/app/controllers/books_controller.rb
@@ -8,17 +8,27 @@ class BooksController < ApplicationController
   end
 
   def create
+    book = Book.create(params[:book])
+    redirect_to(book)
   end
 
   def new
+    @book = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update_attributes(params[:book])
+    redirect_to(book)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to(books_path)
   end
 end
