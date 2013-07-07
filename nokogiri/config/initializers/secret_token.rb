54850799ff14078692e02cac4d8724add1542547
Cold Head <certainthings@gmail.com>
2013-07-08 09:22:19 +1000

    Nokogiri

---
+++ nokogiri/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nokogiri::Application.config.secret_token = '1b5f4f048566c03f11bb499527e1661a1173a367dd2580301ca2bd45b04c124d0f70a80c7fe36ecaa53f1e753ca2049fbf8f541878166349262f9aa2e7e4746e'
