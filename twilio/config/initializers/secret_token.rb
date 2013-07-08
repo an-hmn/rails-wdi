6cc84de5628b61e11a806632af3d21e4db5c2185
Cold Head <certainthings@gmail.com>
2013-07-09 09:59:57 +1000

    Gems, database

---
+++ twilio/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twilio::Application.config.secret_token = '70df6ebb19d0b368b878c638b030b4b77ed6d582521d6c804f89252ae5fad07b6d8d72d1bb56a0eeeda45bb7d70f50eec9fb6fe408ba3708812e902f46236c7d'
