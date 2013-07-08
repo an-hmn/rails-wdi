53fafc05df8897a0cdd7c57fd0209ceff5f9931b
Cold Head <certainthings@gmail.com>
2013-07-08 15:16:15 +1000

    Logins and bears, oh my

--- tunr/app/models/user.rb
+++ tunr/app/models/user.rb
@@ -2,15 +2,17 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  image      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  image           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :image
+  has_secure_password
+  attr_accessible :name, :image, :password, :password_confirmation
   has_many :mixtapes
   has_and_belongs_to_many :albums
 end
