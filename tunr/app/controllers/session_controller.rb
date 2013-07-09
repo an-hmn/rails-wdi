797da7d4c624752034f1f93a37a51b8b2663ae64
Cold Head <certainthings@gmail.com>
2013-07-09 15:10:56 +1000

    Logout, intellinav

--- tunr/app/controllers/session_controller.rb
+++ tunr/app/controllers/session_controller.rb
@@ -3,9 +3,17 @@ class SessionController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    user = User.where(:name => params[:username]).first
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to(root_path)
+    else
+      redirect_to(login_path)
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to(root_path)
   end
 end
