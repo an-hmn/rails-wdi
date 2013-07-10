5868ce75d6e7d1376e6f5fb1b96e9ce342424cec
Cold Head <certainthings@gmail.com>
2013-07-10 12:56:39 +1000

    admin/login filters

--- tunr/app/controllers/users_controller.rb
+++ tunr/app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  before_filter :check_if_logged_in, :except => [:new, :create]
+  before_filter :check_if_admin, :only => [:index, :destroy]
+
   def index
-    @users = @auth.is_admin? ? User.all : []
+    @users = User.all
   end
 
   def new
@@ -31,10 +34,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    if @auth.is_admin?
-      user = User.find(params[:id])
-      user.destroy
-    end
+    user = User.find(params[:id])
+    user.destroy
     redirect_to(users_path)
+  end
+
+  private
+  def check_if_logged_in
+    redirect_to(root_path) if @auth.nil?
+  end
+
+  def check_if_admin
+    redirect_to(root_path) if @auth.nil? || !@auth.is_admin?
   end
 end
