0a089632ce79907fec543f38abb7bb96da1df45e
Cold Head <certainthings@gmail.com>
2013-08-26 12:34:31 +1000

    human readable version

--- numb3rs/app/models/Number.rb
+++ numb3rs/app/models/Number.rb
@@ -1,2 +1,16 @@
 class Number
+  def self.bin2dec(binary)
+    # binary.split('').each_with_index.map {|c, i| (c.to_i) * (2**i)}.reduce(:+)
+
+    digits = binary.split ''
+    digits.reverse!
+
+    power = -1
+    values = digits.map do |c|
+      power = power + 1
+      (c.to_i) * (2 ** power)
+    end
+
+    values.reduce :+
+  end
 end
