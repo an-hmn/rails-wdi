fb6bc3c3919b6f252199583b25dcb240d03bf955
Cold Head <certainthings@gmail.com>
2013-08-26 16:31:12 +1000

    dec2L27

--- numb3rs/app/assets/javascripts/l27.js
+++ numb3rs/app/assets/javascripts/l27.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+  // Calculate decimal version of L27 text and update the decimal <textarea>.
   $('#l27').keyup(function () {
     var text = $(this).val();
     text = text.toLowerCase();
@@ -12,7 +14,7 @@ $(document).ready(function () {
     for (var i = 0; i < chars.length; i++) {
       var c = chars[i];
 
-      if (c === '-') return; // - counts for 0.
+      if (c === '-') continue; // - counts for 0.
 
       var value = c.charCodeAt() - 96; // Char code for the character before 'a'
       decimal = decimal + value * Math.pow(27, i);
@@ -21,7 +23,25 @@ $(document).ready(function () {
     $('#decimal').val(decimal);
   });
 
-  $('#decimal').keypress(function () {
-    // HOMEWORK LOLZ
+  // Calculate the L27 text for the decimal entered and update the L27 <textarea>.
+  $('#decimal').keyup(function () {
+    var decimal = $(this).val();
+    n = parseInt(decimal);
+
+    // He's the man, who's name you'd love to touuuch...
+    var max_power = Math.ceil(Math.log(n) / Math.log(27)); // Or some other big number.
+    var digits = [];
+
+    for (var i = max_power; i >= 0; i--) {
+      var quotient = Math.floor(n / Math.pow(27, i));
+      if (quotient > 0) {
+        digits.push(String.fromCharCode(96 + quotient));
+        n = n - (quotient * Math.pow(27, i));
+      } else {
+        digits.length && digits.push('-')
+      }
+    }
+
+    $('#l27').val(digits.join(''));
   });
 });
