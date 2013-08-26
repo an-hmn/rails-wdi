8ce13dcecb7ebd334ea78c671f0add97098fc3ae
Cold Head <certainthings@gmail.com>
2013-08-26 11:53:03 +1000

    numb3rs gemfile

---
+++ numb3rs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Numb3rs::Application.config.secret_token = 'a760ac7a752fb1bb10463ded4e330a3b6068acb4e4e98c6af18b15d2bbf74c813fc772f87bb3a934e32fb82ce21d028aa00fcd8391107f06637127d8b9f2131d'
